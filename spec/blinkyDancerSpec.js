11420cb29e0f1d8ad1e6098505bf6f2b0b177352
girikalra <girikalra@gmail.com>
2018-01-23 12:32:43 -0800

    added some tests for bouncy dancer

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -30,5 +30,44 @@ describe('blinkyDancer', function() {
       clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(2);
     });
+    
+  });
+});
+
+describe('bouncyDancer', function() {
+
+  var bouncyDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    bouncyDancer = new BouncyDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(bouncyDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a node defined', function() {
+    expect(bouncyDancer.$node).to.not.equal(undefined);
+  });
+
+  it('should have inherited properties from Dancer', function() {
+    expect(bouncyDancer.gravitate).to.not.equal(undefined);
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(bouncyDancer, 'step');
+      expect(bouncyDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(bouncyDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(bouncyDancer.step.callCount).to.be.equal(2);
+    });
+    
   });
 });
