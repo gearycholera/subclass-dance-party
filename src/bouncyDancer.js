a3dce87b0c61a1f95ac155c8ed8e8d377b48091e
girikalra <girikalra@gmail.com>
2018-01-22 19:23:48 -0800

    added new bouncy dancer, implemented line up feature, added background image

---
+++ src/bouncyDancer.js
@@ -0,0 +1,14 @@
+var BouncyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="dancerBouncy"></span>');
+  this.setPosition(top, left);
+};
+
+BouncyDancer.prototype = Object.create(Dancer.prototype);
+BouncyDancer.prototype.constructor = BouncyDancer;
+BouncyDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+
+  this.left += 100;
+  this.setPosition(this.top, this.left);
+};
--- src/init.js
+++ src/init.js
@@ -22,12 +22,22 @@ $(document).ready(function() {
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
   
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
+    console.log(window.dancers);
+  });
+
+  $('.lineUp').on('click', function(event) {
+    var x = 50;
+    for (var i = 0; i < window.dancers.length; i++) {
+      window.dancers[i].setPosition(500, x);
+      x += 50;
+    }
   });
 });
 
